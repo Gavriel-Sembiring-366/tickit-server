7dd6ec843a599624f458b48a4b8610f0c337dd90
Gavriel-Sembiring <gavrielsembiring366@gmail.com>
2024-12-02 12:25:32 +0700

    change to singleton mode

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,5 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+// Check if we're in a serverless environment
+const prisma = global.prisma || new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma; // Reuse PrismaClient instance in development
+}
+
+export default prisma;
