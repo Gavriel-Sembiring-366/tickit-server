cba966165972402622d16f687e9408a7afcd3bb4
Gavriel-Sembiring <gavrielsembiring366@gmail.com>
2024-12-02 17:53:45 +0700

    update db.config.js to address error

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,10 +1,27 @@
 import { PrismaClient } from '@prisma/client';
 
-// Check if we're in a serverless environment
-const prisma = global.prisma || new PrismaClient();
+// Ensure we have only one PrismaClient instance
+let prisma;
 
+if (!global.prisma) {
+  prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // Enable detailed logs for debugging
+  });
+
+  // Attach the PrismaClient instance to the global object in non-production environments
+  if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prisma;
+  }
+} else {
+  prisma = global.prisma;
+}
+
+// Ensure prisma is disconnected during hot reload in development
 if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma; // Reuse PrismaClient instance in development
+  process.once('SIGUSR2', async () => {
+    await prisma.$disconnect();
+    process.kill(process.pid, 'SIGUSR2');
+  });
 }
 
 export default prisma;
