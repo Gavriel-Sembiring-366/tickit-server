b61b99ea742c2b682c72518284a29ca8442eddc5
jeaneaprill <jeaneaprilania@gmail.com>
2024-12-15 09:56:15 +0700

    add jadwal to route

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ import authController from "./auth/auth.controller.js"
 import userController from "./user/user.controller.js"
 import filmController from "./film/film.controller.js"
 import bioskopController from "./bioskop/bioskop.controller.js"
+import jadwalController from "./jadwal/jadwal.controller.js"
 
 // import unknownEndPoint from './middleware/unknownEndpoint.js';
 const app = express()
@@ -22,6 +23,7 @@ app.use('/api/auth/', authController)
 app.use('/api/', userController)
 app.use('/api/', filmController)
 app.use('/api/', bioskopController)
+app.use('/api/', jadwalController)
 
 app.listen(port, () => {
   console.log(`Tickit is listening on port ${port}`)
