8cdebb1b1f1ed3f817705e887bdd0d1b9cf882c7
jeaneaprill <jeaneaprilania@gmail.com>
2024-12-17 01:05:24 +0700

    get image by id

--- server/src/image/image.service.js
+++ server/src/image/image.service.js
@@ -26,3 +26,32 @@ export const getAllImage = async () => {
         url: `${process.env.SUPABASE_URL}/storage/v1/object/public/${bucketName}/${file.name}`,
     }));
 };
+
+
+export const getImageById = async (id) => {
+    const bucketName = 'movie_images';
+    if (!id) {
+        throw new Error('Image ID is required');
+    }
+
+    const response = await fetch(
+        `${process.env.SUPABASE_URL}/storage/v1/object/public/${bucketName}/${id}`,
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.SUPABASE_ANON_KEY}`, // Use the Anon Key for public access
+            },
+        }
+    );
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(`Failed to fetch image: ${error.message}`);
+    }
+
+    // Construct public URL
+    return {
+        name: id,
+        url: `${process.env.SUPABASE_URL}/storage/v1/object/public/${bucketName}/${id}`,
+    };
+};
